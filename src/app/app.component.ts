df308c4f04f67a1f4a97a209d3932ec9e0c4c3e6
pdcr859 <olivier.thibaud@labanquepostale.fr>
2024-02-27 14:34:30 +0000

    communicartion

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +6,25 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
+
+  texteMFE: string = "";
+  
+  @HostListener("document:echange", ["$event"])
+  echangeTexte($event: any) {
+    this.texteMFE = $event.detail;
+  }
+  
   title = 'host-app';
+
+  ajoutPanier(): void {
+    const event: CustomEvent = new CustomEvent('Ajoutpanier', {});
+    document.dispatchEvent(event);
+  }
+
+  supPanier(): void {
+    const event: CustomEvent = new CustomEvent('Suppanier', {});
+    document.dispatchEvent(event);
+  }
+  
+
 }
